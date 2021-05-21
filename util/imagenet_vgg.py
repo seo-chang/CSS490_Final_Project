7fe4887b19ca09c734acaf9858d7c6a8eaab2c07
Jaeha Choi <jaeha220@gmail.com>
2021-05-20 20:24:39 -0700

    Add placeholder

---
+++ util/imagenet_vgg.py
@@ -0,0 +1,15 @@
+import PIL.Image
+import numpy as np
+import torch.utils.data
+
+
+class TempData(torch.utils.data.Dataset):
+
+    def __init__(self):
+        pass
+
+    def __getitem__(self, item):
+        pass
+
+    def __len__(self):
+        pass
